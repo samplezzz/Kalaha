856bc8395fff3e2e6c1a46740e121ba2f693544b
Page Dresser <pagedresser@gmail.com>
2023-03-07 15:23:49 +0100

    FE: draft making a move

--- frontend/src/app/components/game-component.ts
+++ frontend/src/app/components/game-component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { GameService } from '../service/game-service';
 
 @Component({
     templateUrl: './game-component.html',
     styleUrls: ['./game-component.scss'],
 })
-export class GameComponent {}
+export class GameComponent {
+    game$: Observable<any>;
+
+    constructor(private gameService: GameService) {
+        this.game$ = this.gameService.game$;
+    }
+
+    move(fromField: number) {
+        this.gameService.move(fromField);
+    }
+}
