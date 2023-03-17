ffae092ea278f0e0f956c7c317388901c61e6eca
Page Dresser <pagedresser@gmail.com>
2023-03-17 13:19:09 +0100

    FE: animate route change

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
+import { animate, query, style, transition, trigger } from '@angular/animations';
 import { Component, HostBinding } from '@angular/core';
 
 import { GameService } from './service/game-service';
 
 @Component({
     selector: 'ka-root',
-    template: '<router-outlet></router-outlet>',
+    template: `<div [@routeTransition]="outlet.isActivated ? outlet.activatedRoute : ''">
+        <router-outlet #outlet="outlet"></router-outlet>
+    </div>`,
+    animations: [
+        trigger('routeTransition', [
+            transition('* => *', [
+                query(':enter', [style({ opacity: 0, position: 'absolute' })], {
+                    optional: true,
+                }),
+                query(':leave', [style({ opacity: 1 }), animate('0.3s', style({ opacity: 0, position: 'absolute' }))], {
+                    optional: true,
+                }),
+                query(':enter', [style({ opacity: 0 }), animate('0.3s', style({ opacity: 1, position: 'relative' }))], {
+                    optional: true,
+                }),
+            ]),
+        ]),
+    ],
 })
 export class AppComponent {
     constructor(private gameService: GameService) {}
