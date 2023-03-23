d1aed07ba3aa82706a45df94b15eda489462d639
Page Dresser <pagedresser@gmail.com>
2023-03-23 17:27:09 +0100

    FE: test 'GameService' that 'should notify about opponent joining'

--- frontend/src/app/service/game-service.spec.ts
+++ frontend/src/app/service/game-service.spec.ts
@@ -63,9 +63,54 @@ describe('GameService', () => {
         expect(spyHttpClient.post).toHaveBeenCalledOnceWith(`http://mockhost:7777/user/start`, {}, jasmine.any(Object));
         expect(spyWebSocketClient.subscribe).not.toHaveBeenCalled();
 
-        mockCreatedGame$.next({ code: 'CREATED-GAME', pods: [], status: 'PLAYING', turn: 'DOWN' });
+        mockCreatedGame$.next({ code: 'CREATED-GAME', pods: [], status: 'NEW', turn: 'DOWN' });
 
         expect(spyWebSocketClient.subscribe).toHaveBeenCalledWith(`/queue/game-CREATED-GAME`, jasmine.any(Function));
+    });
+
+    it('should notify about opponent joining', () => {
+        let gameState: Game | undefined | null = null;
+        service.game$.subscribe((g) => (gameState = g));
+
+        expect(gameState).withContext('The game should be initially undefined.').toBeUndefined();
+        expect(service.role).toBeUndefined();
+
+        service.createNewGame().subscribe();
+        mockCreatedGame$.next({ code: 'CREATED-GAME', pods: [], status: 'NEW', turn: 'DOWN' });
+
+        if (gameState) {
+            expect(gameState)
+                .withContext('The game should be initialized after #createNewGame() is called.')
+                .toBeDefined();
+            expect(gameState)
+                .withContext('The game should be initialized to the object returned by the API.')
+                .toEqual({ code: 'CREATED-GAME', pods: [], status: 'NEW', turn: 'DOWN' });
+        } else {
+            fail('Game should be defined.');
+        }
+        expect(service.role).withContext('User role should not be initialized after creating a game.').toBeUndefined();
+
+        let joinedGame: Game | undefined;
+        service.awaitOpponent().subscribe((g) => (joinedGame = g));
+
+        expect(joinedGame)
+            .withContext('#awaitOpponent() should not notify until a game is joined on the backend')
+            .toBeUndefined();
+        expect(service.role)
+            .withContext('User role should not be initialized when awaiting the opponent.')
+            .toBeUndefined();
+
+        if (gameState) {
+            mockRemoteGame$.next(Object.assign(gameState, { status: 'PLAYING' }));
+            expect(joinedGame)
+                .withContext('#awaitOpponent() should notify when a game is joined on the backend')
+                .toBeDefined();
+            expect(service.role)
+                .withContext('User role should be initialized to DOWN after the opponent joins.')
+                .toBe('DOWN');
+        } else {
+            fail('Game should be defined.');
+        }
     });
 });
 
